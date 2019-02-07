931b31142c4c2e8b83f66c05029e07b4f411fb15
Mudit Mittal <mmudit30@gmail.com>
2019-02-08 00:02:13 +0530

    Files

---
+++ Assets/animation_atk_btn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class animation_atk_btn : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject vBtnObj;
+    public Animator animModel;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animModel.Play("atk03");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        animModel.Play("idle");
+    }
+
+    // Use this for initialization
+    void Start () {
+        vBtnObj = GameObject.Find("mybtn_atk");
+        vBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        animModel.GetComponentInChildren<Animator>();
+        animModel.Play("idle");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/animation_die_btn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class animation_die_btn : MonoBehaviour, IVirtualButtonEventHandler{
+
+    public GameObject vBtnObj;
+    public Animator animModel;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animModel.Play("die");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        animModel.Play("idle");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        vBtnObj = GameObject.Find("mybtn_die");
+        vBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        animModel.GetComponentInChildren<Animator>();
+        animModel.Play("idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
